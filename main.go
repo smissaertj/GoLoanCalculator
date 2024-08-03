cfe7bbf1c574702da088beb2a084bf8c5306b143
Joeri Smissaert <smissaertj@gmail.com>
2024-08-03 19:18:43 +0400

    calculatePayment()

--- main.go
+++ main.go
@@ -12,26 +12,25 @@ func main() {
 	principal := flag.Float64("principal", 0, "The amount borrowed.")
 	interest := flag.Float64("interest", 0, "The interest rate.")
 	payment := flag.Float64("amount", 0, "Monthly repayment amount.")
-	period := flag.Uint("period", 0, "Loan term; amount of time in months to pay off the loan.")
+	periods := flag.Uint("periods", 0, "Loan term; amount of time in months to pay off the loan.")
 	flag.Parse()
 
 	// Determine the missing parameter that needs to be calculated
 	switch {
 	case !isFlagPassed("principal"):
-		*principal = math.Round(calculatePrincipal(*interest, *payment, *period))
+		*principal = calculatePrincipal(*interest, *payment, *periods)
 		fmt.Printf("Your loan principal is = %f!", *principal)
 	case !isFlagPassed("interest"):
 		// calculateInterest()
 		fmt.Println("Calculating Interest")
-	case !isFlagPassed("period"):
-		*period = calculatePeriod(*payment, *principal, *interest)
-		fmt.Printf("Your loan period is = %d months!", *period)
+	case !isFlagPassed("periods"):
+		*periods = calculatePeriods(*payment, *principal, *interest)
+		fmt.Printf("Your loan period is = %d months!", *periods)
 	case !isFlagPassed("amount"):
-		// calculateAmount()
-		fmt.Println("Calculating Amount")
+		*payment = calculatePayment(*principal, *interest, *periods)
+		fmt.Printf("Your monthly payment = %f!", *payment)
 	}
 
-	fmt.Println("Principal", *principal, "Interest", *interest, "Period", *period, "Payment", *payment)
 }
 
 func isFlagPassed(flagName string) bool {
@@ -52,11 +51,18 @@ func calculatePrincipal(interest, amount float64, period uint) float64 {
 	i := convertInterest(interest)
 	numerator := amount
 	denominator := (i * math.Pow(1+i, float64(period))) / (math.Pow(1+i, float64(period)) - 1)
-	return numerator / denominator
+	return math.Round(numerator / denominator)
 }
 
-func calculatePeriod(principal, interest, payment float64) uint {
+func calculatePeriods(principal, interest, payment float64) uint {
 	i := convertInterest(interest)
 	n := math.Log(payment/(payment-i*principal)) / math.Log(1+i)
-	return uint(math.Ceil(n)) // Round up to the next whole number
+	return uint(math.Ceil(n))
+}
+
+func calculatePayment(principal, interest float64, periods uint) float64 {
+	i := convertInterest(interest)
+	numerator := i * math.Pow(1+i, float64(periods))
+	denominator := math.Pow(1+i, float64(periods)) - 1
+	return math.Ceil(principal * (numerator / denominator))
 }
