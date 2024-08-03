b6aea7c24a3c3d7f06e2ce7af384607036d26783
Joeri Smissaert <smissaertj@gmail.com>
2024-08-03 19:05:33 +0400

    Updated amount flag to payment

--- main.go
+++ main.go
@@ -11,27 +11,27 @@ func main() {
 	// Parse provided parameters
 	principal := flag.Float64("principal", 0, "The amount borrowed.")
 	interest := flag.Float64("interest", 0, "The interest rate.")
-	amount := flag.Float64("amount", 0, "Monthly repayment amount.")
+	payment := flag.Float64("amount", 0, "Monthly repayment amount.")
 	period := flag.Uint("period", 0, "Loan term; amount of time in months to pay off the loan.")
 	flag.Parse()
 
 	// Determine the missing parameter that needs to be calculated
 	switch {
 	case !isFlagPassed("principal"):
-		*principal = math.Round(calculatePrincipal(*interest, *amount, *period))
+		*principal = math.Round(calculatePrincipal(*interest, *payment, *period))
 		fmt.Printf("Your loan principal is = %f!", *principal)
 	case !isFlagPassed("interest"):
 		// calculateInterest()
 		fmt.Println("Calculating Interest")
 	case !isFlagPassed("period"):
-		*period = calculatePeriod(*amount, *principal, *interest)
+		*period = calculatePeriod(*payment, *principal, *interest)
 		fmt.Printf("Your loan period is = %d months!", *period)
 	case !isFlagPassed("amount"):
 		// calculateAmount()
 		fmt.Println("Calculating Amount")
 	}
 
-	fmt.Println("Principal", *principal, "Interest", *interest, "Period", *period, "Amount", *amount)
+	fmt.Println("Principal", *principal, "Interest", *interest, "Period", *period, "Payment", *payment)
 }
 
 func isFlagPassed(flagName string) bool {
@@ -55,8 +55,8 @@ func calculatePrincipal(interest, amount float64, period uint) float64 {
 	return numerator / denominator
 }
 
-func calculatePeriod(principal, interest, amount float64) uint {
+func calculatePeriod(principal, interest, payment float64) uint {
 	i := convertInterest(interest)
-	n := math.Log(amount/(amount-i*principal)) / math.Log(1+i)
+	n := math.Log(payment/(payment-i*principal)) / math.Log(1+i)
 	return uint(math.Ceil(n)) // Round up to the next whole number
 }
